9429d3382a193a10a47a70ad9b73a8f6a631b040
Joachim Nordholmen <joanord97@gmail.com>
2023-06-01 23:03:10 +0200

    stuff

---
+++ app/redirect/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RedirectPage = () => {
+  return <div>Hello world</div>;
+};
+
+export default RedirectPage;
---
+++ components/login/loginButton.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+
+export const LoginButton = () => {
+  const onClick = async (e: any) => {
+    const url = "https://api.nordeaopenbanking.com/personal/v5/authorize";
+
+    const headers = {
+      "x-ibm-client-id": process.env.NEXT_PUBLIC_CLIENT_ID || "",
+      redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL || "",
+      state: "fakeState",
+      "x-ibm-client-secret": process.env.NEXT_PUBLIC_CLIENT_SECRET || "",
+      signature: "SKIP_SIGNATURE_VALIDATION_FOR_SANDBOX",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    const body = {
+      account_list: ["ALL"],
+      authentication_method: "BANKID_NO",
+      country: "NO",
+      duration: 3600,
+      language: "en",
+      max_th_history: 10,
+      redirect_uri:
+        "https://developer.nordeaopenbanking.com/console/" +
+        process.env.NEXT_PUBLIC_CONSOLE_ID,
+      scope: [
+        "ACCOUNTS_BALANCES",
+        "ACCOUNTS_BASIC",
+        "ACCOUNTS_DETAILS",
+        "ACCOUNTS_TRANSACTIONS",
+        "CARDS_INFORMATION",
+        "CARDS_TRANSACTIONS",
+        "PAYMENTS_MULTIPLE",
+      ],
+      state: "fakeState",
+    };
+
+    const result = await fetch(url, {
+      method: "POST",
+      redirect: "follow",
+      cache: "no-cache",
+      headers: headers,
+      body: JSON.stringify(body),
+    });
+
+    console.log("result", result);
+  };
+
+  return <button onClick={onClick}>Login</button>;
+};
